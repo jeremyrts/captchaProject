d729e6159f5c182afd841030efeb00c5dee9b33f
jeremyrts <jeremy.ratsimandresy@gmail.com>
2019-05-19 14:30:12 +0200

    Add panneauRouge as second level for Panneau

---
+++ sources/fr/upem/captcha/images/panneaux/rouge/PanneauRouge.java
@@ -0,0 +1,23 @@
+package fr.upem.captcha.images.panneaux.rouge;
+
+import java.util.ArrayList;
+
+import fr.upem.captcha.images.panneaux.Panneau;
+
+public class PanneauRouge extends Panneau {
+
+	public PanneauRouge() {
+		imagesURL = new ArrayList<>();
+		numberURL = 0;
+		// A l'initialisation, on ajoute toutes les URL d'images du package et on incrémente le nombre
+		imagesURL.add(PanneauRouge.class.getResource("panneauRouge1.jpg"));
+		numberURL++;
+		imagesURL.add(PanneauRouge.class.getResource("panneauRouge2.jpg"));
+		numberURL++;
+		imagesURL.add(PanneauRouge.class.getResource("panneauRouge3.jpg"));
+		numberURL++;
+		imagesURL.add(PanneauRouge.class.getResource("panneauRouge4.jpg"));
+		numberURL++;
+	}
+
+}
