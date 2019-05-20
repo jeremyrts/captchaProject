dc72169ab44259a8fe19edfa35d11eaa34a776f6
jeremyrts <jeremy.ratsimandresy@gmail.com>
2019-05-20 23:04:18 +0200

    Add level check and new content

--- sources/fr/upem/captcha/Main.java
+++ sources/fr/upem/captcha/Main.java
@@ -26,27 +26,35 @@ import javax.swing.JLabel;
 import javax.swing.JTextArea;
 
 import fr.upem.captcha.images.Images;
+import fr.upem.captcha.images.animaux.chien.Chien;
+import fr.upem.captcha.images.animaux.chien.shiba.Shiba;
+import fr.upem.captcha.images.animaux.chien.shiba.akita.Akita;
 import fr.upem.captcha.images.panneaux.Panneau;
+import fr.upem.captcha.images.panneaux.rouge.PanneauRouge;
+import fr.upem.captcha.images.panneaux.rouge.rond.PanneauRond;
+import fr.upem.captcha.images.panneaux.rouge.rond.sensInterdit.SensInterdit;
 import fr.upem.captcha.images.voitures.Voiture;
 import fr.upem.captcha.ui.MainUI;
 
 public class Main {
 	
-	private static ArrayList<URL> selectedImages = new ArrayList<URL>();
-	private static int numberOfImages = 3;
 	private static int numberGoodImages;
-	private Object themeObject;
+	private static String theme;
+	private static int level = 1;
 	
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException, InstantiationException, IllegalAccessException, ClassNotFoundException{
 		// Affichage de l'UI
+		int res = 2;
 		MainUI ui = new MainUI();
 		JFrame frame = new JFrame ("Captcha");
 		
 		// Premier niveau par défaut
-		final String theme = getTheme(1, "none");
-		int res = 2;
+		System.out.println("Niveau : "+level);
+		theme = getTheme(level, "none");
+		numberGoodImages = getNumberOfGoodImage();
+		String currentTheme = theme;
 		try {
-			ui.run(theme, frame);
+			ui.run(level, theme, frame, numberGoodImages);
 			
 		} catch (InstantiationException e) {
 			e.printStackTrace();
@@ -56,33 +64,57 @@ public class Main {
 			e.printStackTrace();
 		}
 		
-		// Vérification : est ce que le captcha est bon ? 
-		
+
 		while (res == 2){
+			//System.out.println(frame.getContentPane().getComponent(frame.getContentPane().getComponentCount() - 2));
 			res = ui.checkResult((JTextArea)frame.getContentPane().getComponent(frame.getContentPane().getComponentCount() - 2));
 		}
-		
-		// NON 
-		// On update la fenêtre avec de nouvelles images 
-
 		if(res == 0) {
 			System.out.println("C'est faux, il faut passer à la difficulté supérieure");
-			System.out.println(getTheme(2, theme));
+			level++;
+			System.out.println("Niveau "+level);
+			
+			theme = getTheme(level, currentTheme);
+			currentTheme = theme;
+			System.out.println(theme);
+			frame.getContentPane().removeAll();
+			frame.repaint();
+			res = 2;
+			ui.run(level, theme, frame, getNumberOfGoodImage());
+			while (res == 2){
+				res = ui.checkResult((JTextArea)frame.getContentPane().getComponent(frame.getContentPane().getComponentCount() - 2));
+			}
+			if(res == 0) {
+				System.out.println("C'est faux, il faut passer à la difficulté supérieure");
+				level++;
+				System.out.println("Niveau "+level);
+				theme = getTheme(level, currentTheme);
+				currentTheme = theme;
+				System.out.println(theme);
+				frame.getContentPane().removeAll();
+				frame.repaint();
+				res = 2;
+				ui.run(level, theme, frame, getNumberOfGoodImage());
+				while (res == 2){
+					res = ui.checkResult((JTextArea)frame.getContentPane().getComponent(frame.getContentPane().getComponentCount() - 2));
+				}
+				if(res == 0) {
+					frame.add(new JTextArea("Acces refusé"));
+				}
+				else if(res == 1){
+					System.out.println("C'est vrai, tu peux accéder au contenu");
+				}
+			}
+			else if(res == 1){
+				System.out.println("C'est vrai, tu peux accéder au contenu");
+				
+			}
 		}
-		
-		// OUI
-		// Message de validation, fin du programme
 		
 		else if(res == 1){
 			System.out.println("C'est vrai, tu peux accéder au contenu");
-			
 		}
 		
-			
-		// Redefinition du thème 
-		// theme = getTheme()
-		
-		// ui.update(theme)
 		
 		
 	}
@@ -91,32 +123,58 @@ public class Main {
 		Random rand = new Random();
 		int value = rand.nextInt(2);
 		String theme = new String();
-		
-		if(currentTheme.equals("none")) {
+		switch(level) {
+		case 1:
+			System.out.println("Niveau 1");
 			switch(value) {
 			case 0: 
-				theme = "fr.upem.captcha.images.animaux.Animal";
+				System.out.println("La value aléatoire chien : "+value);
+				theme = Chien.class.getName();
 				break;
 			case 1:
-				theme = "fr.upem.captcha.images.panneaux.Panneau";
+				System.out.println("La value aléatoire panneauRouge : "+value);
+				theme = PanneauRouge.class.getName();
 				break;
 			default:
-				theme = "fr.upem.captcha.images.panneaux.Panneau";
-			}
-		}
-		
-		if(level == 2) {
-			switch(currentTheme) {
-			case "fr.upem.captcha.images.animaux.Animal":
-				theme = "fr.upem.captcha.images.animaux.chien";
 				break;
+			}
+			break;
+	
+		case 2:
+			System.out.println("Niveau 2");
+			if(currentTheme.equals(Chien.class.getName())) {
+				theme = Shiba.class.getName();
+			}
+			else {
+				theme = PanneauRond.class.getName();
+			}
+			break;
 			
-			case "fr.upem.captcha.images.panneaux.Panneau":
-				theme = "fr.upem.captcha.images.panneaux.rouge";
-				break;
+		case 3:
+			System.out.println("Niveau 3");
+			if(currentTheme.equals(Shiba.class.getName())) {
+				theme = Akita.class.getName();
 			}
+			else {
+				theme = SensInterdit.class.getName();
+			}
+			break;
+		default:
+			break;
+			
 		}
 		return theme;
 		
+	}
+	
+	private static int getNumberOfGoodImage() {
+		Random rand = new Random();
+		int value = rand.nextInt(2);
+		System.out.println("Number good image : "+value);
+		while(value==0) {
+			value = rand.nextInt(2);
+		}
+		System.out.println("Number good image : "+value);
+		return value;
 	}
 }
