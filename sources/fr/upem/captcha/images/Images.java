2dd1da42c5ff2b30281222f203bcf4a957dd2bd1
jeremyrts <jeremy.ratsimandresy@gmail.com>
2019-05-20 23:03:45 +0200

    From interface to global class

--- sources/fr/upem/captcha/images/Images.java
+++ sources/fr/upem/captcha/images/Images.java
@@ -1,16 +1,56 @@
 package fr.upem.captcha.images;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
-public interface Images {
+public class Images {
 	
-	List<URL> getPhotos();
+	protected List<URL> imagesURL; 
+	protected int numberURL;
 	
-	List<URL> getRandomPhotosURL();
+	public List<URL> getPhotos() {
+		return this.imagesURL;
+	}
+
+	public List<URL> getRandomPhotosURL() {
+		List<URL> randomLink = new ArrayList<URL>();
+		Random rand = new Random();
+		int number = 0;
+		
+		// Combien d'image vont s'afficher
+		while (number == 0) {
+			number = rand.nextInt(5);
+		}
+		// Quelles images vont s'afficher
+		for (int i = 0; i < number; i++) {
+			randomLink.add(this.getRandomPhotoURL());
+		}
+		
+		return randomLink;
+	}
+
+	public URL getRandomPhotoURL() {
+		Random rand = new Random();
+		int value = rand.nextInt(numberURL);
+		while (value == 0) {
+			value = rand.nextInt(numberURL);
+		}
+		return this.imagesURL.get(value);
+	}
+
+	public boolean isPhotoCorrect(URL url) {
+		if(this.imagesURL.contains(url)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 	
-	URL getRandomPhotoURL();
-	
-	boolean isPhotoCorrect(URL url);
+	public String getPackageName() {
+		return this.getClass().getPackage().getName();
+	}
 
 }
